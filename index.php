d5d6b60f1fb1f3fd8223e8f398750124a34afff1
Federico <federico.scarpati1989@gmail.com>
2021-01-28 17:32:12 +0100

    Impostazione con titolo e paragrafo

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
     <body>
         <?php
         $badword = $_GET["badword"];
-        $paragrafo = "Un file di testo di piccole dimensioni che viene copiato sul disco rigido da un server di pagine Web e costituisce la carta d’identità dell’utente";
+        $paragrafo = "Un file di testo di piccole dimensioni che viene copiato sul disco rigido da un server di pagine Web e costituisce la carta d’identità dell’utente.";
         $lunghezzaParagrafo = strlen($paragrafo);
         $nuovoParagrafo = str_replace($badword, "***", $paragrafo);
         ?>
-        <h1><?php echo $nuovoParagrafo . " " . $lunghezzaParagrafo ?></h1>
+        <h1><?php echo $nuovoParagrafo ?></h1>
+        <p><?php echo  "La lunghezza del paragrafo è di ". " " . $lunghezzaParagrafo . " " . "caratteri" ?></p>
     </body>
 </html>
